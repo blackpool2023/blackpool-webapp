ccd822366145a3b1f00f2f5d3140556866e19c86
jahanvi-dev <jahanvi.akolia@gmail.com>
2023-07-18 18:22:46 +0530

    home page design

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
-import "pages/styles/global.scss"
+import React, { useState, createContext } from 'react';
+import "pages/assets/scss/global.scss";
+import "pages/assets/scss/header.scss";
+import "pages/assets/scss/variable.scss";
+import "pages/assets/scss/home.scss";
 import MainLayout from "pages/layouts/MainLayout";
+import messagesEn from 'pages/helpers/locales/en.json';
+import messagesDe from 'pages/helpers/locales/de.json';
 
+const Context = createContext();
 function MyApp({ Component, pageProps }) {
+  const menuMessages = {
+    en: { ...messagesEn },
+    de: { ...messagesDe },
+    // fr: { ...messagesFr },
+    // pt: { ...messagesPt },
+  };
+
+  const [locale, setLocale] = useState('en');
+  const [messages, setMessages] = useState(menuMessages['en']);
+
+  const switchLanguage = lang => {
+    setLocale(lang);
+    setMessages(menuMessages[lang]);
+  };
+
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <Context.Provider value={{ locale, switchLanguage, messages }}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </Context.Provider>
   );
 }
 
---
+++ pages/helpers/constants/ImageConstant.js
@@ -0,0 +1,5 @@
+export const images = {
+    logo: require('pages/assets/images/bp-with-slogan.png'),
+    gallery1: require('pages/assets/images/gallery-image1.jpeg'),
+    gallery2: require('pages/assets/images/gallery-image2.jpeg')
+};
