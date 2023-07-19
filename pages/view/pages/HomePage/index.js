bf158d6433ea1b30e66fa9569cda0a0cf25bc74d
jahanvi-dev <jahanvi.akolia@gmail.com>
2023-07-19 12:20:43 +0530

    update layout component

--- pages/view/pages/HomePage/index.js
+++ pages/view/pages/HomePage/index.js
@@ -43,7 +43,7 @@ const HomePage = () => {
                     <KeyboardArrowUpIcon />
                 </button>
                 {/* banner */}
-                {/* <CarouselSlider /> */}
+                <CarouselSlider />
                 <Box className="container">
                     {/* about */}
                     <Box className="about-main">
@@ -51,10 +51,10 @@ const HomePage = () => {
                         <Typography variant='p' className='site-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography>
                     </Box>
                     {/* image */}
-                    {/* <Gallery /> */}
+                    <Gallery />
                 </Box>
                 {/* gallery */}
-                {/* <Box className="bg-black">
+                <Box className="bg-black">
                     <Box className="container">
                         <Typography variant='h2' className='site-heading'>Gallery</Typography>
                         <Box className="lightbox-main">
@@ -108,7 +108,7 @@ const HomePage = () => {
                             </Grid>
                         </Box>
                     </Box>
-                </Box> */}
+                </Box>
             </Box>
         </>
     );
