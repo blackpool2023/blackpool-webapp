c0f30af5ada5061ff1a59f208c6b727d0a1be411
jahanvi-dev <jahanvi.akolia@gmail.com>
2023-07-28 16:18:27 +0530

    service page

--- views/layouts/MainLayout/index.js
+++ views/layouts/MainLayout/index.js
@@ -1,13 +1,35 @@
+
+import React, { useEffect, useRef, useState } from 'react';
 import Header from "views/layouts/MainLayout/Header";
 import Footer from "views/layouts/MainLayout/Footer";
 
 const MainLayout = ({ children }) => {
+    const [isPlay, setIsPlay] = useState(true)
+    const videoRef = useRef(null);
+    useEffect(() => {
+        // Once the component mounts, start playing the video
+        videoRef.current.play();
+        setTimeout(() => {
+            setIsPlay(false)
+        }, 7000);
+    }, []);
     return (
-        <div>
-            <Header />
-            <main>{children}</main>
-            <Footer />
-        </div>
+        <>
+            {isPlay ? (
+                <div style={{ background: 'black' }} >
+                    <video ref={videoRef} autoPlay muted loop style={{ height: '100vh' }} >
+                        <source src='/assets/images/banner/logomotion.mp4' />
+                    </video>
+                </div>
+            ) : (
+                <div>
+                    <Header />
+                    <main>{children}</main>
+                    <Footer />
+                </div>
+            )}
+        </>
+
     );
 };
 
