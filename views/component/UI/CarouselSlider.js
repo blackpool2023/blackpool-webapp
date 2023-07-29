698454076bd47caba82362c01b728230a42a414b
jahanvi-dev <jahanvi.akolia@gmail.com>
2023-07-29 14:45:04 +0530

    changes in video

--- views/component/UI/CarouselSlider.js
+++ views/component/UI/CarouselSlider.js
@@ -55,8 +55,11 @@ const CarouselSlider = () => {
             </div>
             <div key={2}>
                 {/* <Link href="#"> */}
-                <video ref={videoRef} autoPlay muted loop>
-                    <source src='/assets/images/banner/bannervdo1.mp4' />
+                <video ref={videoRef} autoPlay muted loop style={{
+                    minHeight: '600px',
+                    width: '100vw',
+                }} >
+                    <source src='/assets/images/banner/bannervdo3.mp4' />
                 </video>
                 {/* </Link> */}
             </div>
