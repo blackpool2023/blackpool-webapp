ad0029392b335bacfbab4c72b17c1b82b89d80f1
jahanvi-dev <jahanvi.akolia@gmail.com>
2023-08-24 18:57:13 +0530

    replace email

--- pages/view/pages/HomePage/index.js
+++ pages/view/pages/HomePage/index.js
@@ -98,7 +98,7 @@ const HomePage = () => {
                     <Link href="https://www.instagram.com/blackpool_hannover/" target="_blank" className="hexagon">
                         <InstagramIcon />
                     </Link>
-                    <Link href="mailto:jahanvi.jnext@gmail.com" className="hexagon">
+                    <Link href="mailto:info@blackpool-hannover.de" className="hexagon">
                         <PermContactCalendarIcon />
                     </Link>
                 </Box>
