da54ddd403729eb195c80f93f2f29e077156cfa0
jahanvi-dev <jahanvi.akolia@gmail.com>
2023-08-31 14:44:59 +0530

    meta title change

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import HomePage from "pages/view/pages/HomePage"
 const index = () => {
     return (
         <>
-            <Helmet title='Home - Blackpool' description='BlackPool – Poolbauer aus Hannover baut deinen Pool in ganz Niedersachsen ✓ Planung, Bau & Wartung ✓ Hochwertige Betonpools' />
+            <Helmet title='Poolbau in Hannover - Schwimmbadbau von BlackPool' description='BlackPool – Poolbauer aus Hannover baut deinen Pool in ganz Niedersachsen ✓ Planung, Bau & Wartung ✓ Hochwertige Betonpools' />
             <HomePage />
         </>
     );
