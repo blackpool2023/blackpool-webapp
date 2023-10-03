1669ce8a7c56ca142cea7d0137a1998d3a793417
Blackpool-HA <146328815+Blackpool-HA@users.noreply.github.com>
2023-10-03 19:50:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import HomePage from "pages/view/pages/HomePage"
 const index = () => {
     return (
         <>
-            <Helmet title='Poolbau in Hannover - Schwimmbadbau von BlackPool' description='Poolbaue aus Hannover - BlackPool baut deinen Pool in ganz Niedersachsen' />
+            <Helmet title='Poolbau in Hannover - Schwimmbadbau von BlackPool' description='Poolbau aus Hannover - BlackPool baut deinen Pool in ganz Niedersachsen' />
             <HomePage />
         </>
     );
